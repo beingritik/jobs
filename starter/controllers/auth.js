9ed271c427838cf0d46c939563c9944af7dc464d
beingritik <ritikjain98.r@gmail.com>
2023-01-23 08:27:46 +0530

    loginn register completed

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,30 +1,49 @@
-const {StatusCodes}= require('http-status-codes');
-const bcrypt = require('bcryptjs')
-const {BadRequestError,UnauthenticatedError}= require('../errors') 
-const User= require('../models/User') ;
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+const User = require("../models/User");
 
 const register = async (req, res) => {
-  console.log("register starts with email",req.body.email);
-  const {password,name,email}= req.body;
-//   const salt = await bcrypt.genSalt(10)
-//   const hashedPassword = await bcrypt.hash(password,salt)
-  const tempUser = {password,name,email};
-  try{
-const user = await User.create({
-  ...tempUser,
-});
-res.status(StatusCodes.CREATED).json({ user });  }
-  catch(err){
-    console.log("errr=",err.message)
+  console.log("register starts with email", req.body.email);
+  try {
+    const user = await User.create({
+      ...req.body,
+    });
+    const token = await user.createJWT();
+    // console.log("token=",token)
+    res.status(StatusCodes.CREATED).json({ user, token: token });
+  } catch (err) {
+    console.log("errr=", err.message);
   }
-  
 };
 
 const login = async (req, res) => {
-  console.log("login");
+  // console.log("login starts with email", req.body.email);
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      throw new BadRequestError("please provide valid info");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+        // console.log("passsss=", isPassowrdMatched);
+    if (!isPasswordMatched) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+    const token = await user.createJWT();
+    res
+      .status(StatusCodes.OK)
+      .json({ user: { name: user.name }, token: token });
+  }
+  catch (err) {
+    console.log("error is=", err.message);
+  }
 };
 
 module.exports = {
-    login,
-    register
-}
+  login,
+  register,
+};
--- starter/models/User.js
+++ starter/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
     ],
-    // unique: true,
+    unique: true,
   },
   password: {
     type: String,
@@ -24,5 +24,21 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
+
+UserSchema.pre('save',async function(){
+const salt = await bcrypt.genSalt(10)
+this.password = await bcrypt.hash(this.password,salt)
+
+})
+
+UserSchema.methods.createJWT= async function(){
+return jwt.sign( {userID:this._id, name:this.name},process.env.JWT_SECRET,{expiresIn:"1h"});
+}
+
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  const isMatch = bcrypt.compare(enteredPassword, this.password)
+  return isMatch;
+};
+
 
 module.exports = mongoose.model("User", UserSchema);
